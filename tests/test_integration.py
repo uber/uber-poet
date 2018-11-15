9cfc0548aed0e1b6adfa70ee836273c1bc8e3cc9
Mahyar McDonald <mahyar@uber.com>
2018-11-14 18:04:31 -0800

    Make integration testing more extensive and skipped by default

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -20,8 +20,20 @@ from pearpoet.genproj import GenProjCommandLine
 from pearpoet.multisuite import CommandLineMultisuite
 
 
+def integration_test(func):
+
+    def do_nothing(arg):
+        pass
+
+    if 'INTEGRATION' in os.environ:
+        return func
+    else:
+        return do_nothing
+
+
 class TestIntegration(unittest.TestCase):
 
+    @integration_test
     def test_flat_genproj(self):
         app_path = os.path.join(tempfile.gettempdir(), 'apps', 'mockapp')
         args = [
@@ -32,6 +44,7 @@ class TestIntegration(unittest.TestCase):
         command.main(args)
         self.assertGreater(os.listdir(app_path), 0)
 
+    @integration_test
     def test_dot_genproj(self):
         test_fixture_path = os.path.join(os.path.dirname(__file__), 'fixtures', 'test_dot.gv')
         app_path = os.path.join(tempfile.gettempdir(), 'apps', 'mockapp')
@@ -43,6 +56,7 @@ class TestIntegration(unittest.TestCase):
         command.main(args)
         self.assertGreater(os.listdir(app_path), 0)
 
+    @integration_test
     def test_flat_multisuite(self):
         app_path = os.path.join(tempfile.gettempdir(), 'apps', 'mockapp')
         args = ["--log_dir", app_path, "--app_gen_output_dir", app_path, "--test_build_only", "--skip_xcode_build"]
@@ -50,12 +64,25 @@ class TestIntegration(unittest.TestCase):
         command.main(args)
         self.assertGreater(os.listdir(app_path), 0)
 
+    @integration_test
     def test_dot_multisuite(self):
         test_fixture_path = os.path.join(os.path.dirname(__file__), 'fixtures', 'test_dot.gv')
         app_path = os.path.join(tempfile.gettempdir(), 'apps', 'mockapp')
         args = [
             "--log_dir", app_path, "--app_gen_output_dir", app_path, "--dot_file", test_fixture_path, "--dot_root",
-            "DotReaderMainModule", "--test_build_only", "--skip_xcode_build"
+            "DotReaderMainModule", "--skip_xcode_build", "--test_build_only"
+        ]
+        command = CommandLineMultisuite()
+        command.main(args)
+        self.assertGreater(os.listdir(app_path), 0)
+
+    @integration_test
+    def test_all_multisuite(self):
+        test_fixture_path = os.path.join(os.path.dirname(__file__), 'fixtures', 'test_dot.gv')
+        app_path = os.path.join(tempfile.gettempdir(), 'apps', 'mockapp')
+        args = [
+            "--log_dir", app_path, "--app_gen_output_dir", app_path, "--dot_file", test_fixture_path, "--dot_root",
+            "DotReaderMainModule", "--skip_xcode_build", "--lines_of_code", "150000"
         ]
         command = CommandLineMultisuite()
         command.main(args)
