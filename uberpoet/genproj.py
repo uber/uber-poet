1db655aede453f4019b85c2c5f9fb7c37a3bf170
Dimitris Koutsogiorgas <dimitris@squareup.com>
2021-04-09 12:49:43 -0700

    Add CocoaPods project generation support

--- uberpoet/genproj.py
+++ uberpoet/genproj.py
@@ -19,7 +19,7 @@ import logging
 import sys
 import time
 
-from . import commandlineutil, projectgen
+from . import buckprojectgen, commandlineutil, cpprojectgen
 from .moduletree import ModuleGenType
 
 
@@ -28,28 +28,33 @@ class GenProjCommandLine(object):
     @staticmethod
     def make_args(args):
         """Parses command line arguments"""
-        arg_desc = 'Generate a fake test project with many buck modules'
+        arg_desc = 'Generate a fake test project with many modules'
 
         parser = argparse.ArgumentParser(description=arg_desc)
 
         parser.add_argument('-o', '--output_directory', required=True, help='Where the mock project should be output.')
         parser.add_argument(
+            '-pgt',
+            '--project_generator_type',
+            choices=['buck', 'cocoapods'],
+            default='buck',
+            required=False,
+            help='The project generator type to use. Supported types are BUCK and CocoaPods. Default is BUCK')
+        parser.add_argument(
             '-bmp',
             '--buck_module_path',
-            required=True,
-            help='The root of the BUCK dependency path of the generated code.')
-
+            help='The root of the BUCK dependency path of the generated code. Only used if BUCK generator type is used.'
+        )
         parser.add_argument(
             '-gt',
             '--gen_type',
             required=True,
             choices=ModuleGenType.enum_list(),
             help='What kind of mock app generation you want.  See layer_types.md for a description of graph types.')
-        parser.add_argument(
-            '-wmo',
-            '--use_wmo',
-            default=False,
-            help='Wether or not to use whole module optimization when building swift modules.')
+        parser.add_argument('-wmo',
+                            '--use_wmo',
+                            default=False,
+                            help='Wether or not to use whole module optimization when building swift modules.')
         parser.add_argument(
             '--print_dependency_graph',
             default=False,
@@ -76,12 +81,12 @@ class GenProjCommandLine(object):
             exit(0)
 
         commandlineutil.del_old_output_dir(args.output_directory)
-        gen = projectgen.BuckProjectGenerator(args.output_directory, args.buck_module_path, use_wmo=args.use_wmo)
+        gen = project_generator_for_arg(args)
 
+        logging.info("Project Generator type: %s", args.project_generator_type)
         logging.info("Generation type: %s", args.gen_type)
         logging.info("Creating a {} module count mock app in {}".format(len(node_list), args.output_directory))
-        logging.info("Example command to generate xcode workspace: $ buck project /{}/App:MockApp".format(
-            args.buck_module_path))
+        logging.info("Example command to generate Xcode workspace: $ {}".format(gen.example_command()))
         gen.gen_app(app_node, node_list, graph_config.lines_of_code)
 
         fin = time.time()
@@ -92,6 +97,17 @@ def print_nodes(node_list):
     edges = [(node.name, dep.name) for node in node_list for dep in node.deps]
     for edge in edges:
         print edge[0], edge[1]
+
+
+def project_generator_for_arg(args):
+    if args.project_generator_type == 'buck':
+        if not args.buck_module_path:
+            raise ValueError("Must supply --buck_module_path when usign the BUCK generator.")
+        return buckprojectgen.BuckProjectGenerator(args.output_directory, args.buck_module_path, use_wmo=args.use_wmo)
+    elif args.project_generator_type == 'cocoapods':
+        return cpprojectgen.CocoaPodsProjectGenerator(args.output_directory, use_wmo=args.use_wmo)
+    else:
+        raise ValueError("Unknown project generator arg: " + str(args.project_generator_type))
 
 
 def main():
